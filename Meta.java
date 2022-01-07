2ec387e3f1c0b749c06036158e954766429ea06c
Ahamed-Ashik <96856514+Ahamed-Ash@users.noreply.github.com>
2022-01-07 17:27:14 +0530

    Meta.java

---
+++ Meta.java
@@ -0,0 +1,13 @@
+package Javademos;
+
+public class Meta {
+    public static void main(String[] args){
+        System.out.println("public class Hello{");
+        System.out.println("public static void main(String[] args{");
+        System.out.println("Hello, World!\");");
+        System.out.println("       }");
+        System.out.println("}");
+    }
+
+
+    }
