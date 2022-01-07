68ebebf8614a8d10fce62c03fe3f69174f634a93
Ahamed-Ashik <96856514+Ahamed-Ash@users.noreply.github.com>
2022-01-07 17:26:26 +0530

    Egg2.java

---
+++ Egg2.java
@@ -0,0 +1,33 @@
+package Javademos;
+
+public class Egg2 {
+    public static void main(String[] args){
+        printUpperbody();
+        printLowerbody();
+        printMiddlebody();
+        printUpperbody();
+        printLowerbody();
+        printMiddlebody();
+        printLowerbody();
+        printUpperbody();
+        printMiddlebody();
+        printLowerbody();}
+        public static void printUpperbody(){
+            System.out.println("_______");
+            System.out.println("/    \\");
+            System.out.println("/     \\");
+        }
+        public static void printMiddlebody(){
+            System.out.println("-'-'-'-'-'-");
+        }
+        public static void printLowerbody(){
+            System.out.println("\\   /");
+            System.out.println("\\  /");
+            System.out.println("_____");
+        }
+
+        
+        
+    
+
+    }
